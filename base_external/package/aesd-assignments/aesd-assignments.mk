700fa70035e45b3804d1b41c82b57ff00cffcc47
Fernando Becerra Tanaka <fernando.becerratanaka@colorado.edu>
2024-11-04 21:17:18 -0600

    Add assignment 9 functionality

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '4c9e264855532b4aba5b505255c6d5df21c95d3a'
+AESD_ASSIGNMENTS_VERSION = '77e55c427987e3abb98c8f40e681ad62c4ec9276'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesdchardriver/aesdchardriver.mk
+++ base_external/package/aesdchardriver/aesdchardriver.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESDCHARDRIVER_VERSION = '4c9e264855532b4aba5b505255c6d5df21c95d3a'
+AESDCHARDRIVER_VERSION = '77e55c427987e3abb98c8f40e681ad62c4ec9276'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
